a964a5b18d3a91feb678bd4466da4b64ecd4546d
Mike V <dev.mikev@gmail.com>
2018-11-29 16:33:37 -0500

    complete stats section

--- js/script.js
+++ js/script.js
@@ -33,9 +33,9 @@ $(document).ready(function() {
         }
     })
 
-
     var skillsTopOffset = $(".skillsSection").offset().top;
-
+    var statsTopOffset = $(".statsSection").offset().top;
+    var countUpFinished = false;
     $(window).scroll(function() {
 
         if (window.pageYOffset > skillsTopOffset - $(window).height() + 200) {
@@ -51,5 +51,17 @@ $(document).ready(function() {
                 }
             });
         }
+
+        if (!countUpFinished && window.pageYOffset > statsTopOffset - $(window).height() + 200) {
+            $(".counter").each(function() {
+                var element = $(this);
+                var endVal = parseInt(element.text());
+        
+                element.countup(endVal);
+            });
+            countUpFinished = true;
+        }
     });
+
+
 });
